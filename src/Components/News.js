58933d9f2fe47a331f1adde42340fc96e316ef8e
Abhay <kabhay849@gmail.com>
2023-01-09 02:41:21 +0530

    lecture 28 completed successfully added previous and next button in newsapp

--- src/Components/News.js
+++ src/Components/News.js
@@ -10,28 +10,48 @@ export class News extends Component {
     super();
     this.state = {
       articles: this.article,  //kind of state variable
-      loading: false
+      loading: false,
+      page: 1,
+      TotalResult:0
     }
 
   }
 
-  async componentDidMount() {   //componentDidMount() is a fn start after everything on app uploaded
-    let response = await fetch('https://newsapi.org/v2/top-headlines?country=in&apiKey=0d22f87577de4cf0b52741caf0309aaf')
-   let  data= await response.json()
+  componentDidMount = async () => {   //componentDidMount() is a fn start after everything on app uploaded
+    let response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=0d22f87577de4cf0b52741caf0309aaf&page=${this.state.page}&pageSize=12`)
+    let data = await response.json()
     //  this.setState({articles:data.article})
-     this.setState({articles:data.articles}) //data.articles is array see sample  output
+    this.setState({ articles: data.articles ,TotalResult:data.totalResults}) //data.articles is array see sample  output
+  }
+  handleprevious = async () => {
+    let response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=0d22f87577de4cf0b52741caf0309aaf&page=${this.state.page - 1}&pageSize=12`)
+    let data = await response.json()
+    this.setState({ articles: data.articles, page: this.state.page - 1 })
+  }
+  handlenext = async () => {
+    // if(this.state.page>Math.ceil(this.state.TotalResult/20)){
+
+    // }
+  let response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=0d22f87577de4cf0b52741caf0309aaf&page=${this.state.page + 1}&pageSize=12`)
+      let data = await response.json()
+      this.setState({ articles: data.articles, page: this.state.page + 1 })
+   
   }
   render() {
-    return (
+    return (<>
       <div className='container'>
         <h1 className='text-center my-5'>News-Adda</h1>
         <div className="row">
           {this.state.articles.map((element) => {             //this.article.map((element)  is also fine but this.state.articles.map((element) is used because we need to change all the things which only done through state
-            return <div className='col' key={element.url}><NewsItems title={element.title} description={element.description} imageUrl={!element.urlToImage? 'https://img.etimg.com/thumb/msid-79969308,width-650,height-488,imgsize-443378,,resizemode-75/istock-1211803628.jpg':element.urlToImage} NewsUrl={element.url} /></div>
+            return <div className='col' key={element.url}><NewsItems title={element.title?element.title.slice(0,72)+'.....':'checkout....'} description={element.description?element.description.slice(0,76)+'...':element.title}  imageUrl={!element.urlToImage ? 'https://thumbs.dreamstime.com/b/t-lymphocytes-attacking-cancer-cell-d-illustration-anticancer-immunity-treatment-concept-t-lymphocytes-attacking-cancer-cell-125858701.jpg' : element.urlToImage} NewsUrl={element.url} /></div>
           })}
         </div>
-
       </div>
+      <div className="container d-flex justify-content-between my-5">
+        <button disabled={this.state.page === 1} className='btn btn-dark' onClick={this.handleprevious}>&laquo; previous</button>
+        <button disabled={Math.ceil(this.state.TotalResult/10)===this.state.page} className='btn btn-dark' onClick={this.handlenext} id='buttonNext'>Next &raquo;</button>
+      </div>
+    </>
     )
   }
 }
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -5,13 +5,13 @@ export class NewsItems extends Component {
     render() {
         let {title,description,imageUrl,NewsUrl}=this.props
         return (
-            <div>
-                <div className="card" style={{width: '18rem'}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+            <div className='d-flex justify-content-center m-3'>
+                <div className="card" style={{width: '18rem' ,height:'60vh'}}>
+                    <img  src={imageUrl} className="card-img-top" alt="..."/>
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
-                            <a href={NewsUrl} className="btn btn-primary btn-sm">View</a>
+                            <a href={NewsUrl} className="btn btn-dark btn-sm">View</a>
                         </div>
                 </div>
             </div>
