b456633311c81631c80788d5d3bbea71faef649d
Abhay <kabhay849@gmail.com>
2023-01-11 22:24:05 +0530

    lecture-32 completed author,time and source (badge) added

--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -3,15 +3,19 @@ import React, { Component } from 'react'
 export class NewsItems extends Component {
 
     render() {
-        let {title,description,imageUrl,NewsUrl}=this.props
+        let { title, description, imageUrl, NewsUrl, Author, time ,source  } = this.props
         return (
             <div className='m-3'>
-                <div className="card"  style={{height:'95vh'}}>
-                    <img src={imageUrl} className="card-img-top" alt="img" style={{height:'35vh'}}/>
-                        <div className="card-body d-flex flex-column justify-content-between">
-                            <p className="card-title fs-4 fw-bold">{title}<p className="card-text fs-6 fw-normal"  >{description}</p></p>
-                            <a href={NewsUrl} className="btn btn-dark btn-lg">Read More</a>
-                        </div>
+                <div className="card" style={{ height: '95vh' }}>
+                    <span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
+                        {source}
+                        <span class="visually-hidden">unread messages</span>
+                    </span>
+                    <img src={imageUrl} className="card-img-top" alt="img" style={{ height: '35vh' }} />
+                    <div className="card-body d-flex flex-column justify-content-between">
+                        <div className="card-title fs-4 fw-bold">{title}<p className="card-text fs-6 fw-normal m-0"  >{description}</p><p className='fs-6 fw-lighter m-0 text-danger'>{Author?Author:'Unknown'}</p><p className='fs-6 fw-light m-0 text-danger'>{time}</p></div>
+                        <a href={NewsUrl} className="btn btn-dark btn-lg">Read More</a>
+                    </div>
                 </div>
             </div>
 
