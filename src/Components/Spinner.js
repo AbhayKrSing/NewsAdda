4057697566faf854a754f7ce2fcedf5ac7db38a8
Abhay <kabhay849@gmail.com>
2023-01-13 18:11:36 +0530

    spinner giving 100vh height

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -3,7 +3,7 @@ import logo from './loader.gif'
 export default class Spinner extends Component {
   render() {
     return (
-      <div className='text-center d-flex align-items-center justify-content-center'>
+      <div className='text-center d-flex align-items-center justify-content-center' style={{height:'100vh'}}>
         <img width={100} className='img-fluid' src={logo} alt="loading..." />
       </div>
     )
