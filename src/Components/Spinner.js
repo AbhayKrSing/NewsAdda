a5fbe60f9cbf29a07caf1bfaf074c573fd7b22a7
Abhay <kabhay849@gmail.com>
2023-01-14 17:38:07 +0530

    spinner.js modified

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -3,7 +3,7 @@ import logo from './loader.gif'
 export default class Spinner extends Component {
   render() {
     return (
-      <div className='text-center d-flex align-items-center justify-content-center' style={{height:'100vh'}}>
+      <div className='text-center d-flex align-items-center justify-content-center vh-100'>
         <img width={100} className='img-fluid' src={logo} alt="loading..." />
       </div>
     )
